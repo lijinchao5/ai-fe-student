63cecdf8560fbba576735c4218a068fd3f40691c
sunhuixian <fairysun749@163.com>
2018-04-08 18:10:17 +0800

    [sunhuixian] #N/A 修改自主练习列表页样式

--- js/nomousPractice/practiceList.js
+++ js/nomousPractice/practiceList.js
@@ -6,7 +6,7 @@
 //});
 var map;
 $(document).ready(function () {
-	map=new Map();
+    map = new Map();
     $('#header').load('../common/header.html');
     $('#nav').load('../common/nav.html');
     $('#footer').load('../common/footer.html');
@@ -17,31 +17,31 @@ $(document).ready(function () {
 
 
 //分类部分
-    var classifyAll = $(".classification:first-of-type li").eq(0);
-    var classifyLi = $(".classification li").length;
+//    var classifyAll = $(".classification:first-of-type li").eq(0);
+//    var classifyLi = $(".classification li").length;
 
 //    点击全部 去掉所有选项的类名 给  “全部”添加类名
-    classifyAll.click(function () {
-        $(".classification li").removeClass("current");
-        $(this).addClass("current");
-    });
+//    classifyAll.click(function () {
+//        $(".classification li").removeClass("current");
+//        $(this).addClass("current");
+//    });
 //  点击切换类名
-    $(".classification li").click(function () {
-        $(this).toggleClass("current");
-        //  如果 点击不是 “全部”，去掉“全部” 的类名；
-        if ($(this) !== classifyAll) {
-            classifyAll.removeClass("current");
-            //  如果 “非全部” 没有类名 给“全部” 添加类名
-            if (!($(".classification li").hasClass("current")) || ($(".classification .current").length == (classifyLi - 1))) {
-                $(".classification li").removeClass("current");
-                classifyAll.addClass("current");
-            }
-        }
-    });
+//    $(".classification li").click(function () {
+//        $(this).toggleClass("current");
+//        //  如果 点击不是 “全部”，去掉“全部” 的类名；
+//        if ($(this) !== classifyAll) {
+//            classifyAll.removeClass("current");
+//            //  如果 “非全部” 没有类名 给“全部” 添加类名
+//            if (!($(".classification li").hasClass("current")) || ($(".classification .current").length == (classifyLi - 1))) {
+//                $(".classification li").removeClass("current");
+//                classifyAll.addClass("current");
+//            }
+//        }
+//    });
 //分类 上下按钮切换
     var downindex = 0;
     $(".down-next").click(function () {
-    	var practiceLen = $(".classification").length;
+        var practiceLen = $(".classification").length;
         $(this).attr("src", "../../images/common/down-0.png");
         $(".up-prev").attr("src", "../../images/common/top-0.png");
         downindex++;
@@ -66,21 +66,21 @@ $(document).ready(function () {
 var currentPage = 0;
 var totalPage = 0;
 var cliindex = 0;
-var claindex=0;
+var claindex = 0;
 //等级切换
 function levelAdd() {
     var url = "dic/findDicByType.do?type=15";
     var param = {};
     var _levelScale = $(".level-scale");
     doAjax("get", url, param, function (data) {
-    	var datatmp={};
-    	datatmp.id="";
-    	datatmp.name="全部";
-    	data.unshift(datatmp);
+        var datatmp = {};
+        datatmp.id = "";
+        datatmp.name = "全部";
+        data.unshift(datatmp);
         for (var i = 0; i < data.length; i++) {
-        	map.put("data"+data[i].id,data[i]);
+            map.put("data" + data[i].id, data[i]);
             var _levelLi = "";
-            _levelLi = $("<li data='"+data[i].id+"'>" + data[i].name + "</li>");
+            _levelLi = $("<li data='" + data[i].id + "'>" + data[i].name + "</li>");
             _levelScale.append(_levelLi);
             _levelLi.click(function () {
                 claindex = $(this).index();
@@ -99,51 +99,51 @@ function classify() {
     var param = {};
     var _curentUl = $(".level-classify");
     doAjax("get", url, param, function (data) {
-        var _classifyUl="";
+        var _classifyUl = "";
         for (var a = 0; a < data.length; a++) {
-        	if(a%6==0){
-        		_classifyUl +=  "<ul class='classification'>" ;
-        	}
-        	if(a==0){
-        		_classifyUl+="<li data='' class='current'>全部</li>";
-        	}
-            map.put("data"+data[a].id,data[a]);
-            _classifyUl += "<li data='"+data[a].id+"'>" + data[a].name + "</li>";
-            if(a%6==5||a==data.length-1){
-            	_classifyUl+="</ul>";
-        	}
+            if (a % 6 == 0) {
+                _classifyUl += "<ul class='classification'>";
+            }
+            if (a == 0) {
+                _classifyUl += "<li data='' class='current'>全部</li>";
+            }
+            map.put("data" + data[a].id, data[a]);
+            _classifyUl += "<li data='" + data[a].id + "'>" + data[a].name + "</li>";
+            if (a % 6 == 5 || a == data.length - 1) {
+                _classifyUl += "</ul>";
+            }
         }
         _curentUl.append(_classifyUl);
         onclickclassifyli();
         if (data.length <= 6) {
-            $(".down-top").css("display", "none")
+            $(".down-top").css("visibility", "hidden")
         } else {
-            $(".down-top").css("display", "block")
+            $(".down-top").css('visibility', 'visible')
         }
     })
 }
 
 
-function onclickclassifyli(){
-	$(".level-classify li").click(function (i,v) {
-		var classname=$(this).attr("class");
-		if(classname=="current"){
-			$(this).removeClass('current');
-		}else{
-			$(".level-classify li").eq(0).removeClass('current');
-			$(this).addClass('current');
-		}
-		//判断选中几个
-		var length=$(".level-classify .current").length;
-		if(length==0){
-			$(".level-classify li").eq(0).addClass('current');
-		}
-		//判断全部是否点中
-		var classnameAll=$(".level-classify li").eq(0).attr("class");
-		if(classnameAll=="current"){
-			$(".level-classify li").removeClass('current');
-			$(".level-classify li").eq(0).addClass('current');
-		}
+function onclickclassifyli() {
+    $(".level-classify li").click(function (i, v) {
+        var classname = $(this).attr("class");
+        if (classname == "current") {
+            $(this).removeClass('current');
+        } else {
+            $(".level-classify li").eq(0).removeClass('current');
+            $(this).addClass('current');
+        }
+        //判断选中几个
+        var length = $(".level-classify .current").length;
+        if (length == 0) {
+            $(".level-classify li").eq(0).addClass('current');
+        }
+        //判断全部是否点中
+        var classnameAll = $(".level-classify li").eq(0).attr("class");
+        if (classnameAll == "current") {
+            $(".level-classify li").removeClass('current');
+            $(".level-classify li").eq(0).addClass('current');
+        }
         initData();
     });
 }
@@ -174,25 +174,25 @@ function addListeners() {
 }
 
 function initData() {
-	var type="";
-	var level="";
+    var type = "";
+    var level = "";
     var _artReport = $(".level-classify");
     var param = {};
     param.rows = 9;
     param.page = cliindex + 1;
-    param.type="";
-    var levelid=$(".level-scale .current").attr("data");
-    var levelmap=map.get("data"+levelid);
-    if(levelmap!=null){
-    	param.level=map.get("data"+levelid).nameVal;
+    param.type = "";
+    var levelid = $(".level-scale .current").attr("data");
+    var levelmap = map.get("data" + levelid);
+    if (levelmap != null) {
+        param.level = map.get("data" + levelid).nameVal;
     }
-    $(".level-classify .current").each(function(){
-    	var typeid=$(this).attr("data");
-    	var typemap=map.get("data"+typeid);
-    	console.log("-------------")
-    	console.log(typemap)
-        if(typemap!=null&&typemap.nameVal!=undefined){
-        	param.type+=typemap.nameVal+",";
+    $(".level-classify .current").each(function () {
+        var typeid = $(this).attr("data");
+        var typemap = map.get("data" + typeid);
+        console.log("-------------")
+        console.log(typemap)
+        if (typemap != null && typemap.nameVal != undefined) {
+            param.type += typemap.nameVal + ",";
         }
     })
     var url = "exercise/findExercisePage.do";
@@ -202,24 +202,25 @@ function initData() {
         var datas = data.rows;
         if (null == datas || datas.length <= 0) {
             var _reportEmpty = "";
-            var _practice=$(".practice-wrap");
+            var _practice = $(".practice-wrap");
             _reportEmpty = $("<div class='empty-all'>" + "<img src='../../images/common/empty.png' alt='没有作业'/>" + "<p class='empty-text'>" + "再点点其他的吧！" + "</p>" + "</div>");
             _practice.html(_reportEmpty);
             $(".paging .up").css('visibility', 'hidden');
             $(".paging .down").css('visibility', 'hidden');
             $("#toggleLi").css('visibility', 'hidden');
         } else {
-        	$("#toggleLi").css('visibility', 'visible');
-        	var _practice=$(".practice-wrap").html('<ul class="text-practice" id="artList"></ul>');
+            $("#toggleLi").css('visibility', 'visible');
+            var _practice = $(".practice-wrap").html('<ul class="text-practice" id="artList"></ul>');
             var _artList = $("#artList");
             _artList.html("");
             for (var i = 0; i < datas.length; i++) {
                 var d = datas[i];
-                var _li = $("<li onclick=goTo('" + d.id + "')>" +
+                var _li = $("<li>" +
                     "<p class='practice-title'>" + d.name + "</p>" +
                     "<p class='practice-info'><span>字数：</span><span>" + d.wordNum + "</span>" +
                     "<span class='pr-classify'>类别：</span><span>" + d.typeName + "</span>" +
                     "</p>" +
+                    "<p onclick=goTo('" + d.id + "') class='start-read'> 开始练习</p>" +
                     "</li>");
                 _artList.append(_li);
             }
@@ -260,8 +261,4 @@ function initData() {
 function goTo(id) {
     window.location.href = "./library.html?id=" + id;
 }
-
-
-
-
 
