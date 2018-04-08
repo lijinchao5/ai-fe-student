17b3954b0ef77f13d9520ee7005bfa157a0e987c
lichunluo <lichunluo92@63.com>
2018-04-08 15:38:55 +0800

    搭建模考模块

--- js/common/simulationExamNav.js
+++ js/common/simulationExamNav.js
@@ -3,8 +3,10 @@ $(function(){
 });
 function initUserInfo(){
     var data = JSON.parse(localStorage.getItem("userInfo"));
-    $(".exam_userName").text(data.name);
+    $("#exam_userName").text(data.name);
+    $("#exam_userClass").text(data.classId);
     $(".number").text(data.nameNum);
+    console.log(data);
     if (null == data.photo || "" == data.photo) {
     } else {
         $(".exam_photo").attr("src", getRootPath() + "file/download.do?type=jpg&id=" + data.photo)
